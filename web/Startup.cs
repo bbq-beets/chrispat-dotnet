66388f3783cffd7a9f90c089a1fcde82074be19c
Chris Patterson <chrispat@github.com>
2019-10-10 10:56:31 -0400

    breaking compliation

--- web/Startup.cs
+++ web/Startup.cs
@@ -28,7 +28,7 @@ namespace web
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
+                options.MinimumSameSitePolicy = SameSiteMode.None
             });
 
 
